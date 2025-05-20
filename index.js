8b86b1cef671ccccece143dcf092b50090fb0fdb
lomaleao <pallomaleaoap@gmail.com>
2025-05-19 21:49:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+<script>
+const button = document.getElementById("myButton");
+const messageContainer = document.getElementById("messageContainer");
+
+button.addEventListener("click", function() {messageContainer.textContent = "Continue tentando, você é capaz!";
+    
+});
+</script>
+
+
+
+
+
